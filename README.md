f86444059be20a27e553133628aa3403a370c77c
Jakub Boukal <www.bagr@gmail.com>
2019-09-28 16:50:07 +0200

    Initialize adds, question and answer models

--- README.md
+++ README.md
@@ -9,5 +9,6 @@ cd CrowdClick
 mkvirtualenv -p python3.6 -a . crowdclick
 pip install -r requirements.txt
 python manage.py migrate
+python manage.py loaddata ad_source/fixtures/*
 python manage.py runserver 0.0.0.0:8000
 ```
--- ad_source/models.py
+++ ad_source/models.py
@@ -1,3 +1,28 @@
 from django.db import models
 
+
 # Create your models here.
+class Advertisement(models.Model):
+    website_link = models.URLField("Website Link")
+    title = models.CharField("Title", max_length=35)
+    description = models.TextField("Description", max_length=100)
+    reward_per_click = models.FloatField("Reward per click")
+    time_duration = models.DurationField("Time duration")
+
+
+class Question(models.Model):
+    RADIO_TYPE = 'RA'
+    SELECT_TYPE = 'SE'
+    QUESTION_TYPES = (
+        (RADIO_TYPE, 'Radio'),
+        (SELECT_TYPE, 'Select'),
+    )
+
+    ad = models.ForeignKey("Advertisement", on_delete=models.CASCADE)
+    title = models.CharField(max_length=50)
+    question_type = models.CharField(max_length=2, choices=QUESTION_TYPES)
+
+
+class Answer(models.Model):
+    question = models.ForeignKey("Question", on_delete=models.CASCADE)
+    title = models.CharField(max_length=50)
