fc4ca407533ef475a64833b51816f4cd9c46608f
RiccardoProdeo <lambdagalois@protonmail.com>
2019-09-29 18:59:31 +0100

    more of the escrow

--- contracts/escrow.sol
+++ contracts/escrow.sol
@@ -4,11 +4,19 @@ import "openzeppelin-solidity/contracts/math/SafeMath.sol";
 
 
 contract RewardEscrow {
-    using Safemath for uint;
-
+    using Safemath for uint256;
+    uint256 public custodyTime;
     address owner;
+    mapping(address => mapping(address => uint256)) public escrowBalance;
+    // tracks the balance of an address associated to its respective escrow address
+    mapping(address => mapping(address => uint256)) public custodyExpiration;
+    //tracks the time before the escrow contract will expire 
 
-    mapping(address => uint256) private _deposits;
+
+  
+
+    
+
 }
 
 
