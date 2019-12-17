2df0011db5e1cde562dd18d78dc39d97c9fc6542
Jakub Boukal <www.bagr@gmail.com>
2019-12-17 22:18:03 +0100

    :sparkles: Initialize unittest

--- ad_source/tests.py
+++ ad_source/tests.py
@@ -1,3 +1,21 @@
-from django.test import TestCase
+from django.urls import reverse
+from rest_framework import status
+from rest_framework.test import APITestCase
 
-# Create your tests here.
+from .models import Subscribe
+
+
+class SubscribeTests(APITestCase):
+    def setUp(self):
+        self.url = reverse('subscribe-list')
+
+    def test_create_subscribe(self):
+        data = {'email': 'foo@bar.com'}
+        response = self.client.post(self.url, data)
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+        self.assertEqual(Subscribe.objects.count(), 1)
+        self.assertEqual(Subscribe.objects.get().email, 'foo@bar.com')
+
+    def test_get_subscribe(self):
+        response = self.client.get(self.url)
+        self.assertEqual(response.status_code, status.HTTP_405_METHOD_NOT_ALLOWED)
--- crowdclick/settings/defaults.py
+++ crowdclick/settings/defaults.py
@@ -100,6 +100,7 @@ REST_FRAMEWORK = {
         'rest_framework.authentication.SessionAuthentication',
         'rest_framework.authentication.BasicAuthentication'
     ),
+    'TEST_REQUEST_DEFAULT_FORMAT': 'json',
 }
 
 
