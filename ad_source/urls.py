90d4c9b02e6230bb6ab6d1ed123779fd93a123fd
Jakub Boukal <www.bagr@gmail.com>
2019-12-17 21:25:56 +0100

    :fire: Remove views and frontend things

--- ad_source/urls.py
+++ ad_source/urls.py
@@ -2,7 +2,7 @@ from django.urls import path, include
 from rest_framework import routers
 from web3auth import urls as web3auth_urls
 
-from . import views, view_sets
+from . import view_sets
 
 router = routers.DefaultRouter()
 router.register(r'task', view_sets.TaskViewSet)
@@ -12,14 +12,6 @@ router.register(r'subscribe', view_sets.SubscribeViewSet)
 
 
 urlpatterns = [
-    path('', views.home, name='landing_page'),
-    path('logout', views.logout, name='logout'),
-    path('sign_in', views.sign_in, name='sign_in'),
-    path('task', views.task, name="task"),
-    path('advertising_matrix', views.advertising_matrix, name="advertising_matrix"),
-    path('earn/<int:ad_id>', views.earn, name="earn"),
-    path('about', views.about, name='about'),
-    path('publish', views.publish, name='publish'),
     path('api/', include(router.urls)),
     path('', include(web3auth_urls)),
 ]
