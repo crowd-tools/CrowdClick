779b64d95fd84d38ee4cd0d5bec9451cd4399f48
Jakub Boukal <www.bagr@gmail.com>
2020-01-09 20:41:19 +0100

    :children_crossing: Use SimpleRouter without DEBUG

--- ad_source/urls.py
+++ ad_source/urls.py
@@ -1,10 +1,16 @@
+from django.conf import settings
 from django.urls import path, include
 from rest_framework import routers
 from web3auth import urls as web3auth_urls
 
 from . import view_sets
 
-router = routers.DefaultRouter()
+if settings.DEBUG:
+    router_class = routers.DefaultRouter
+else:
+    router_class = routers.SimpleRouter
+
+router = router_class()
 router.register(r'task', view_sets.TaskViewSet)
 router.register(r'task/(?P<task_id>\d+)/answer', view_sets.TaskViewSet)
 router.register(r'question', view_sets.QuestionViewSet)
