4500aba65ec292040a59d9bec56711ad94444486
Jakub Boukal <www.bagr@gmail.com>
2020-01-12 15:09:53 +0100

    :pencil: Update apiary with task creation

--- apiary/hooks.js
+++ apiary/hooks.js
@@ -1,10 +1,10 @@
 const hooks = require('hooks');
 
-hooks.before('Task Collection  > Create a new task', (transaction) => {
-  transaction.request.headers.Authorization = 'Basic: YWRtaW46YWRtaW4K';
+hooks.before('Task Collection > Create a new task', (transaction) => {
+  transaction.request.headers.Authorization = 'Basic YWRtaW46YWRtaW4=';
 });
 
-hooks.before('Task Collection  > List All Tasks', (transaction) => {
-  transaction.request.headers.Authorization = 'Basic: YWRtaW46YWRtaW4K';
+hooks.before('Answer Collection > Create answer for task', (transaction) => {
+  transaction.request.headers.Authorization = 'Basic YWRtaW46YWRtaW4=';
 });
 
