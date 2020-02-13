f80ac6b25e6a6697c7d7dd5452522702a68caeff
Jakub Boukal <www.bagr@gmail.com>
2020-02-13 22:40:04 +0100

    :arrow_down: Remove imagekit from installed apps

--- crowdclick/settings/defaults.py
+++ crowdclick/settings/defaults.py
@@ -37,7 +37,6 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'imagekit',
 
     'rest_framework',
     'web3auth.apps.Web3AuthConfig',
