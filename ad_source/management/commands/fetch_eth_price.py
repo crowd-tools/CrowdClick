9399d20309e47bb1c193dc517cbc2b669f4287a8
Jakub Boukal <www.bagr@gmail.com>
2020-04-07 22:26:21 +0200

    :sparkles: Extend Fetch ETH Price command to dictionary

--- ad_source/management/commands/fetch_eth_price.py
+++ ad_source/management/commands/fetch_eth_price.py
@@ -1,9 +1,11 @@
+import datetime
+
 import requests
 from django.core.cache import cache
 from django.core.management.base import BaseCommand
 
 URL = 'https://min-api.cryptocompare.com/data/price?fsym={from_symbol}&tsyms={to_symbol}'
-CACHE_KEY = 'ETH-to-USD'
+CACHE_KEY = 'ETH-PRICES'
 
 
 class Command(BaseCommand):
@@ -12,8 +14,34 @@ class Command(BaseCommand):
     def add_arguments(self, parser):
         parser.add_argument('--from', type=str, default='ETH')
         parser.add_argument('--to', type=str, default='USD')
+        parser.add_argument('--print', action='store_true', default=False,
+                            help=f'If specified, prints `{CACHE_KEY}` only')
+        parser.add_argument('--drop', action='store_true', default=False,
+                            help=f'If specified, drops `{CACHE_KEY}` only')
 
     def handle(self, *args, **options):
-        response = requests.get(URL.format(from_symbol=options['from'], to_symbol=options['to']))
+        # Print
+        if options.get('print'):
+            print(cache.get(CACHE_KEY))
+            return
+        # Drop
+        if options.get('drop'):
+            value = cache.get(CACHE_KEY)
+            if value:
+                cache.delete(CACHE_KEY)
+                print(f"Dropped: {value}")
+            return
+        # Fetch
+        # https://min-api.cryptocompare.com/data/price?fsym=ETH&tsyms=USD
+        url = URL.format(from_symbol=options['from'], to_symbol=options['to'])
+        response = requests.get(url)
         data = response.json()
-        cache.set(CACHE_KEY, data['USD'])
+
+        cache_dict = cache.get(CACHE_KEY, {})
+        if 'eth_prices' not in cache_dict:
+            cache_dict['eth_prices'] = {}
+        cache_dict.update({"last_updated": datetime.datetime.now().isoformat()})
+        for key, value in data.items():
+            cache_dict['eth_prices'].update({key: value})
+
+        cache.set(CACHE_KEY, cache_dict)
