ae7e08f482559959019f1532b1bd9be9dbe8e8ed
Jakub Boukal <www.bagr@gmail.com>
2020-04-23 22:55:32 +0200

    :white_check_mark: Add test for X-Frame-Option DENY

--- ad_source/tests/view_sets/test_task.py
+++ ad_source/tests/view_sets/test_task.py
@@ -8,7 +8,7 @@ class TestTaskView(APITestCase):
     TASK_DATA = {
         "title": "Check our site",
         "description": "Awesome site. Go check it now and earn crypto",
-        "website_link": "example.com",
+        "website_link": "does_not_exist.com",
         "reward_per_click": 0.001,
         "time_duration": "00:00:30",
         "spend_daily": 1,
@@ -59,25 +59,42 @@ class TestTaskView(APITestCase):
 
     @responses.activate
     def test_create_task_admin(self):
-        responses.add(responses.GET, 'http://example.com',
+        responses.add(responses.GET, 'http://does_not_exist.com',
                       body=self.OG_DATA, status=200)
         self.client.login(username='admin', password='admin')
         response = self.client.post(self.url, data=self.TASK_DATA)
         data = response.json()
         self.assertEqual(response.status_code, 201)
-        self.assertEqual(data['website_link'], 'http://example.com/')
+        self.assertEqual(data['website_link'], 'http://does_not_exist.com/')
         self.assertEqual(data['id'], 3)
 
     @responses.activate
-    def test_create_task_admin_with_second_missing_og_data_is_none(self):
-        responses.add(responses.GET, 'http://example.com',
+    def test_create_task_admin_with_second_missing_og_data(self):
+        responses.add(responses.GET, 'http://does_not_exist.com',
                       body=self.OG_DATA, status=200)
-        responses.add(responses.GET, 'http://example.com',
+        responses.add(responses.GET, 'http://does_not_exist.com',
                       body=self.OG_DATA_2, status=200)
         self.client.login(username='admin', password='admin')
         self.client.post(self.url, data=self.TASK_DATA)
         response = self.client.post(self.url, data=self.TASK_DATA)
         self.assertEqual(response.status_code, 201)
         data = response.json()
-        self.assertEqual(data['website_link'], 'http://example.com/')
+        self.assertEqual(data['website_link'], 'http://does_not_exist.com/')
         self.assertIsNone(data['og_image_link'])
+
+    def test_create_task_admin_with_connection_error(self):
+        self.client.login(username='admin', password='admin')
+        response = self.client.post(self.url, data=self.TASK_DATA)
+        data = response.json()
+        self.assertEqual(['Connection error for http://does_not_exist.com'], data['website_link'])
+
+    @responses.activate
+    def test_create_task_admin_with_iframe_forbidden(self):
+        responses.add(responses.GET, 'http://does_not_exist.com',
+                      body=self.OG_DATA, status=200,
+                      headers={'X-Frame-Options': 'DENY'},)
+        self.client.login(username='admin', password='admin')
+        response = self.client.post(self.url, data=self.TASK_DATA)
+        data = response.json()
+        self.assertEqual(response.status_code, 201)
+        self.assertEqual('Website has strict X-Frame-Options: deny', data['warning_message'])
