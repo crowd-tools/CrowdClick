2dd6c74a464fde10c8f36be47cdc71077c8b91af
Jakub Boukal <www.bagr@gmail.com>
2020-06-26 19:39:45 +0200

    🍱 Update string representation on models

--- ad_source/models.py
+++ ad_source/models.py
@@ -28,6 +28,11 @@ class Task(models.Model):
     class Meta:
         ordering = ['-reward_per_click']
 
+    def __str__(self):
+        if self.title:
+            return f"Task({self.title})"
+        return "Task"
+
     @property
     def reward_usd_per_click(self):
         eth_to_usd = cache.get(FETCH_ETH_PRICE_CACHE_KEY)
@@ -48,7 +53,7 @@ class Question(models.Model):
     question_type = models.CharField(max_length=2, choices=QUESTION_TYPES, default=SELECT_TYPE)
 
     def __str__(self):
-        return "Question(%s, title=%s)" % (self.title, self.question_type)
+        return f"Question({self.title})"
 
 
 class Option(models.Model):
@@ -64,8 +69,12 @@ class SelectedOption(models.Model):
 
 
 class AnsweredQuestion(models.Model):
-    question = models.ForeignKey(Question, on_delete=models.CASCADE)
+    question = models.ForeignKey(Question, related_name='answered_questions', on_delete=models.CASCADE)
     selected_option = models.ManyToManyField(SelectedOption, related_name='answered_questions')
+
+    def __str__(self):
+        options = ', '.join(self.selected_option.values_list('option__title', flat=True))
+        return f"AnsweredQuestion({self.question}, {options})"
 
 
 class Answer(models.Model):
@@ -73,6 +82,9 @@ class Answer(models.Model):
     answered_questions = models.ManyToManyField(AnsweredQuestion, related_name='answers')
     user = models.ForeignKey(User, related_name='answers', on_delete=models.CASCADE)
     timestamp = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f'Answer({self.task}, user={self.user})'
 
 
 class Subscribe(models.Model):
