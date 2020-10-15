40e69950c2708f19e6616fdc152511d0879765bf
Jakub Boukal <www.bagr@gmail.com>
2020-10-16 01:02:05 +0200

    Add endpoint for server config

--- ad_source/view_sets.py
+++ ad_source/view_sets.py
@@ -223,3 +223,13 @@ class RewardViewSet(viewsets.ModelViewSet):
                 "error": "Reward already created"
             }
             return Response(data=data, status=status.HTTP_400_BAD_REQUEST)
+
+
+class ServerConfigViewSet(mixins.ListModelMixin, viewsets.ViewSet):
+    def list(self, request, *args, **kwargs):
+        if not request.user.is_authenticated:
+            return Response(status=status.HTTP_403_FORBIDDEN)
+        data = {
+            'public_key': settings.ACCOUNT_OWNER_PUBLIC_KEY
+        }
+        return Response(data)
