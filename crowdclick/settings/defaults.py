4de90c1918750469db73595965d69b94347be9c1
Jakub Boukal <www.bagr@gmail.com>
2020-10-25 17:28:02 +0100

    👌 Update default Goerli endpoint in default settings

--- crowdclick/settings/defaults.py
+++ crowdclick/settings/defaults.py
@@ -109,7 +109,6 @@ AUTH_NONCE_CHOICES = '0123456789abcdef'
 AUTH_NONCE_LENGTH = 32
 LOGIN_REDIRECT_URL = '/'
 
-INFURA_ROPSTEN_ENDPOINT = ''
 web3_config_namedtuple = namedtuple('Web3_Config', ['endpoint', 'contract_address', 'public_key', 'private_key'])
 WEB3_CONFIG = {
     'mumbai': web3_config_namedtuple(
@@ -119,7 +118,7 @@ WEB3_CONFIG = {
         private_key='a' * 64,
     ),
     'goerli': web3_config_namedtuple(
-        endpoint='https://goerli.net',
+        endpoint='https://goerli.infura.io/v3/' + 'a' * 32,
         contract_address='0xA3d2C3F78652B93311363bBDEc3d16867F6D5C29',
         public_key='0xDd2179e8D8755f810CdAe4a474F7c53F371FbB6A',
         private_key='a' * 64,
