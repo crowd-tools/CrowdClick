3e0fafcdc4bcc18c470e68811d9757a6c51fb1a6
Jakub Boukal <www.bagr@gmail.com>
2021-03-20 01:35:33 +0100

    🚀 Initialize deployment to staging server

---
+++ deploy.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+# This script runs automatically only on stage environment
+
+set -x  # Output entering commands
+
+# Activate env, change directory to project root
+source ~/.virtualenvs/crowdclick_stage/bin/activate
+cd ~/crowd_click_stage/
+export DJANGO_SETTINGS_MODULE=crowdclick.settings.stage
+
+# Pull updates
+git checkout stage -f
+git pull
+
+# Pip dependencies
+pip install -U pip
+pip install -r requirements.txt
+
+# Go Django!
+python manage.py migrate
+python manage.py collectstatic --no-input
+
+# Restart supervisor
+supervisorctl restart crowdclick_stage
