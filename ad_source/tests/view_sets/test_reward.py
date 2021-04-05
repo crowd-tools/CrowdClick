c403ec89a261fb95628f253eba49433e4800ba40
Jakub Boukal <www.bagr@gmail.com>
2021-04-05 21:25:13 +0200

    ✅ Add mock for Web3 when creating reward

--- ad_source/tests/view_sets/test_reward.py
+++ ad_source/tests/view_sets/test_reward.py
@@ -32,21 +32,23 @@ class RewardViewSetTestCase(APITestCase):
 
         w3_checksum_mock.side_effect = web3_checksum_side_effect
         w3_send_transaction_mock.return_value = b'a' * 5
+        with patch('ad_source.tasks.update_task_is_active_balance.delay') as mock_task:
+            self.client.login(username='admin', password='admin')
+            response = self.client.post(self.url)
+            self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+            self.assertEqual(response.json(), {'tx_hash': '6161616161'})
 
-        self.client.login(username='admin', password='admin')
-        response = self.client.post(self.url)
-        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
-        self.assertEqual(response.json(), {'tx_hash': '6161616161'})
+            self.assertEqual(w3_checksum_mock.call_count, 2)
+            self.assertEqual(w3_send_transaction_mock.call_count, 1)
 
-        self.assertEqual(w3_checksum_mock.call_count, 2)
-        self.assertEqual(w3_send_transaction_mock.call_count, 1)
+            # test reward again
+            response = self.client.post(self.url)
+            self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
+            self.assertEqual(response.json(), {"error": "Reward already created"})
+            self.assertEqual(w3_checksum_mock.call_count, 2)
+            self.assertEqual(w3_send_transaction_mock.call_count, 1)
 
-        # test reward again
-        response = self.client.post(self.url)
-        self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
-        self.assertEqual(response.json(), {"error": "Reward already created"})
-        self.assertEqual(w3_checksum_mock.call_count, 2)
-        self.assertEqual(w3_send_transaction_mock.call_count, 1)
+            mock_task.assert_called_once_with(self.task.id)
 
     def test_create_reward_without_answers(self):
         self.client.login(username='admin', password='admin')
