debba6af9645a623123a985438d2c1b0d98e8db9
Jakub Boukal <www.bagr@gmail.com>
2021-04-10 16:10:13 +0200

    🩹 Fix passing argument to celery task

--- ad_source/tasks.py
+++ ad_source/tasks.py
@@ -7,8 +7,15 @@ from . import models, celery, web3_providers
 logger = logging.getLogger(__name__)
 
 
-@celery.app.task
-def update_task_is_active_balance(task_id: int = 4) -> dict:
+@celery.app.task(bind=True)
+def update_task_is_active_balance(self, task_id: int = 4) -> dict:
+
+    # Could be wrapped:
+    # try:
+    #     ...
+    # except web3.Exception as exc:
+    #     # Retry in 5 minutes.
+    #     self.retry(countdown=60 * 5, exc=exc)
 
     task = models.Task.objects.get(id=task_id)
     logger.info(f'Got task to update id: {task}')
