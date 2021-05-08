0ad093082256e7b5dba4ed847b66387dea2dc76e
Jakub Boukal <www.bagr@gmail.com>
2021-05-08 14:58:30 +0200

    ⬆️  Upgrade dependencies

--- crowdclick/settings/defaults.py
+++ crowdclick/settings/defaults.py
@@ -44,6 +44,8 @@ MIDDLEWARE = [
 
 ROOT_URLCONF = 'crowdclick.urls'
 
+DEFAULT_AUTO_FIELD = 'django.db.models.AutoField'
+
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
